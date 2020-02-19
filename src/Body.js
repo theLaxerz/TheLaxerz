df16de41677c353c9d4fce4f2c67fa7c5757cdd2
Aaron F. Lax <aaron.lax@icloud.com>
2020-02-18 20:09:00 -0600

    Changed from Default to Under Construction Version .01

---
+++ src/Body.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function Body() {
+    return (
+        <div className="Body">
+            <div className="AppHeaderTxt">
+                <p>The future of Technology is ever-changing, it is our job to stay on top of it.</p>
+            </div>
+            <div className="AppHeaderTxt">
+                <p>We are currently <i>Under-Construction</i> so come back soon.</p>
+            </div>
+            <div className="AppHeaderTxt">
+                <address>Aaron Lax:<a className="AppHeaderTxt" href="mailto:support@thelaxerz.us">support@thelaxerz.us</a></address>
+            </div>
+        
+        </div>
+        );
+      }          
+      
+      
+export default Body;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Body from './Body';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Body />, document.getElementById('body'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
