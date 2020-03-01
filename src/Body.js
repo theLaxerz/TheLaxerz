0bfc71456c2fae9766c1acbac7631adf692ba5d8
Aaron F. Lax <aaron.lax@icloud.com>
2020-03-01 13:42:10 -0600

    Some minor changes to the UI, before going full out.

--- src/Body.js
+++ src/Body.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
+import './Body.css';
 
 function Body() {
     return (
-        <div className="Body">
+        <div className="BodyHeader">
             <div className="AppHeaderTxt">
                 <p>The future of Technology is ever-changing, it is our job to stay on top of it.</p>
             </div>
             <div className="AppHeaderTxt">
-                <p>We are currently <i>Under-Construction</i> so come back soon.</p>
+                <p>theLaxerz is currently <i>Under-Construction</i> but this is a brief introduction to us.</p>
             </div>
             <div className="AppHeaderTxt">
                 <address>Aaron Lax:<a className="AppHeaderTxt" href="mailto:support@thelaxerz.us">support@thelaxerz.us</a></address>
